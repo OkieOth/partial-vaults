c1e54d814b904fa744a6a914b7037fe3e113798e
Eiko Thomas <13103714+OkieOth@users.noreply.github.com>
2025-07-02 23:04:49 +0200

    project init
    wip

---
+++ cmd/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"github.com/okieoth/pvault/cmd/sub"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "pvault",
+	Short: "Tool to partial encrypt and decrypt JSON or YAML files",
+	Long:  `Tool to partial encrypt and decrypt JSON or YAML files, compatible to Ansible Vault spec.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Please call this with one of the provided sub-commands")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(sub.VersionCmd)
+}
+
+func main() {
+	rootCmd.Execute()
+}
