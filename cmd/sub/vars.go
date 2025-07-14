bc24478cb82df86d2e487c02db1ebe0f3f1e3609
Eiko Thomas <13103714+OkieOth@users.noreply.github.com>
2025-07-14 10:41:29 +0200

    make password a required flag

--- cmd/sub/vars.go
+++ cmd/sub/vars.go
@@ -12,8 +12,9 @@ var password string
 func initDefaultFlags(cmd *cobra.Command) {
 	cmd.Flags().StringVarP(&input, "input", "i", "", "Path to the input file (required)")
 	cmd.Flags().StringVarP(&output, "output", "o", "stdout", "Output to create, default is stdout")
-	cmd.MarkFlagRequired("input")
+	cmd.Flags().StringVarP(&password, "password", "p", "", "Password (required)")
 	cmd.Flags().StringSliceVarP(&keys, "key", "k", []string{}, "Keys to include in the processing. For nested keys the dot notated styles is to use. Can be given multiple times")
 	cmd.Flags().BoolVarP(&interactive, "interactive", "a", false, "Interactive mode")
-	cmd.Flags().StringVarP(&password, "passord", "p", "", "Password")
+	cmd.MarkFlagRequired("input")
+	cmd.MarkFlagRequired("password")
 }
